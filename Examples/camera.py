4e938c5c8555f8a267455653a829601a86a6839a
Andrew <andrewb1230@gmail.com>
2024-04-15 12:47:21 +0200

    Added camera.py

---
+++ Examples/camera.py
@@ -0,0 +1,10 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(2)
+for i in range(3):
+    camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+camera.stop_preview()
