4a3e6e8bff1e619ea46c25e0b58788ca7a9e4de5
Andrew <andrewb1230@gmail.com>
2024-04-17 11:02:55 +0200

    Added camera close.

--- Examples/bno08x.py
+++
@@ -1,8 +0,0 @@
-import time
-import board
-import busio
-import adafruit_bno08x
-from adafruit_bno08x.i2c import BNO08X_I2C
-
-i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
-bno = BNO08X_I2C(i2c)
--- Examples/camera.py
+++ Examples/camera.py
@@ -8,3 +8,4 @@ sleep(2)
 for i in range(3):
     camera.capture('/home/pi/Desktop/image%s.jpg' % i)
 camera.stop_preview()
+camera.close()
