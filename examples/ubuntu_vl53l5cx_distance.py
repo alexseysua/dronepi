ef37b0b5fe2a5e71ccc8a27b47b8a037da6656b0
Andrew123098 <andrewb1230@gmail.com>
2024-08-13 15:29:40 +0200

    Fix ubuntu ToF sensor script.

--- examples/ubuntu_vl53l5cx_distance.py
+++ examples/ubuntu_vl53l5cx_distance.py
@@ -1,5 +1,5 @@
 import time
-import numpy
+import numpy as np
 from vl53l5cx.vl53l5cx import VL53L5CX
 
 driver = VL53L5CX()
@@ -11,53 +11,54 @@ if not alive:
 print("Initialising...")
 t = time.time()
 driver.init()
+time.sleep(1)
 print(f"Initialised ({time.time() - t:.1f}s)")
 
 # Set resolution to 4x4
 driver.set_resolution(4 * 4)
+time.sleep(0.5)
 
 # Set ranging frequency to 90Hz
 driver.set_ranging_frequency_hz(90)
+time.sleep(0.5)
 
+#NOTE: This function causes sensor to not work. Ignore it.
 # Set integration time to 2ms
-driver.set_integration_time_ms(2)
+#driver.set_integration_time_ms(1)
+#time.sleep(0.5)
 
 # Set ranging mode to continuous
 driver.set_ranging_mode(1)
+time.sleep(0.5)
 
 # Ranging:
 driver.start_ranging()
+print("Start Ranging")
 
 prev_time = 0.0
 curr_time = time.time()
 while True:
-    print("Loop")
-    #if driver.check_data_ready():
-    ranging_data = driver.get_ranging_data()
+    if driver.check_data_ready():
+        data = driver.get_ranging_data()
+        arr = np.flipud(np.array(data.distance_mm[0:16]))
+        arr = arr.reshape((4,4))
 
-    # As the sensor is set in 4x4 mode by default, we have a total
-    # of 16 zones to print. For this example, only the data of first zone are
-    # print
+        # Scale view relative to the furthest distance
+        distance = arr.max()
 
-    arr = numpy.flipud(numpy.array(ranging_data.distance_mm)).astype('float64')[0,0:16]
-    arr = arr.reshape((4, 4))
+        # Scale view to a fixed distance
+        distance = 512
 
-    # Scale view relative to the furthest distance
-    distance = arr.max()
+        # Scale and clip the result to 0-255
+        arr *= (255.0 / distance)
+        arr = np.clip(arr, 0, 255)
 
-    # Scale view to a fixed distance
-    distance = 512
+        # Force to int
+        arr = arr.astype('uint8')
+        print(arr)
 
-    # Scale and clip the result to 0-255
-    arr *= (255.0 / distance)
-    arr = numpy.clip(arr, 0, 255)
-
-    # Force to int
-    arr = arr.astype('uint8')
-    print(arr)
-
-    curr_time = time.time()
-    FPS = 1 / (curr_time - prev_time)
-    print("\nFPS: %0.2f" % FPS,"\n")
-    prev_time = curr_time
+        curr_time = time.time()
+        FPS = 1 / (curr_time - prev_time)
+        print("\nFPS: %0.2f" % FPS,"\n")
+        prev_time = curr_time
     time.sleep(0.001)  # Avoid polling *too* fast
