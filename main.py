47f891afa886f2a55ddd9a32804a5d7a12fa8fc5
Andrew <andrewb1230@gmail.com>
2024-08-13 15:39:17 +0200

    Make separate ubuntu and rpi versions of main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import time
-import numpy
+import numpy as np
 import board
 import busio
 import RPi.GPIO as GPIO
-import vl53l5cx_ctypes as vl53l5cx
+from vl53l5cx.vl53l5cx import VL53L5CX
 import argparse
 from pmw3901 import PMW3901, BG_CS_FRONT_BCM, BG_CS_BACK_BCM
 from ina260.controller import Controller
@@ -24,21 +24,39 @@ from adafruit_bno08x.i2c import BNO08X_I2C
 
 ''' DISTANCE SENSOR SETUP '''
 print("Uploading firmware to VL53L5CX, please wait...")
-vl53 = vl53l5cx.VL53L5CX()
-time.sleep(1)
-print("Done!")
-vl53.set_ranging_mode(vl53l5cx.RANGING_MODE_CONTINUOUS)
-vl53.set_resolution(4 * 4)
-time.sleep(1)
+driver = VL53L5CX()
 
-# This is a visual demo, so prefer speed over accuracy
-vl53.set_ranging_frequency_hz(90)
-time.sleep(1)
+alive = driver.is_alive()
+if not alive:
+    raise IOError("VL53L5CX Device is not alive")
 
-vl53.set_integration_time_ms(2)
+print("Initialising...")
+t = time.time()
+driver.init()
 time.sleep(1)
-vl53.start_ranging()
-time.sleep(1)
+print(f"Initialised ({time.time() - t:.1f}s)")
+
+# Set resolution to 4x4
+driver.set_resolution(4 * 4)
+time.sleep(0.5)
+
+# Set ranging frequency to 90Hz
+driver.set_ranging_frequency_hz(90)
+time.sleep(0.5)
+
+#NOTE: This function causes sensor to not work. Ignore it.
+# Set integration time to 2ms
+#driver.set_integration_time_ms(1)
+#time.sleep(0.5)
+
+# Set ranging mode to continuous
+driver.set_ranging_mode(1)
+time.sleep(0.5)
+
+# Ranging:
+driver.start_ranging()
+print("Start Ranging")
+
 
 ''' MOTION SENSOR SETUP '''
 parser = argparse.ArgumentParser()
@@ -84,10 +102,11 @@ while True:
     time.sleep(0.0001)
     
     ## DISTANCE SENSOR
-    if vl53.data_ready():
-        data = vl53.get_data()
-        arr = numpy.flipud(numpy.array(data.distance_mm)).astype('float64')[0, 0:16].reshape((4, 4))
-    
+    if driver.check_data_ready():
+        data = driver.get_ranging_data()
+        arr = np.flipud(np.array(data.distance_mm[0:16]))
+        arr = arr.reshape((4,4))
+
         # Scale view relative to the furthest distance
         distance = arr.max()
 
@@ -96,19 +115,20 @@ while True:
 
         # Scale and clip the result to 0-255
         arr *= (255.0 / distance)
-        arr = numpy.clip(arr, 0, 255)
+        arr = np.clip(arr, 0, 255)
 
+        # Force to int
         arr = arr.astype('uint8')
         print(arr)
     
     ## MOTION SENSOR
-#    try:
-#        x, y = flo.get_motion()
-#    except RuntimeError:
-#        continue
-#    tx += x
-#    ty += y
-#    print("Motion: %0.2f %0.2f, x: %0.2f y %0.2f" % (x, y, tx, ty))
+    try:
+        x, y = flo.get_motion()
+    except RuntimeError:
+        continue
+    tx += x
+    ty += y
+    print("Motion: %0.2f %0.2f, x: %0.2f y %0.2f" % (x, y, tx, ty))
 
     ## IMU SENSOR
     print("Acceleration:")
