f08e4db3737f031b603318024522791340b3310f
Andrew <andrewb1230@gmail.com>
2024-08-13 14:01:22 +0200

    Fix ubuntu ToF distance test script.

--- examples/ubuntu_vl53l5cx_distance.py
+++ examples/ubuntu_vl53l5cx_distance.py
@@ -1,7 +1,6 @@
 import time
-
+import numpy
 from vl53l5cx.vl53l5cx import VL53L5CX
-
 
 driver = VL53L5CX()
 
@@ -14,34 +13,54 @@ t = time.time()
 driver.init()
 print(f"Initialised ({time.time() - t:.1f}s)")
 
+# Set resolution to 4x4
+driver.set_resolution(driver.VL53L5CX_RESOLUTION_4X4)
+
+# Set ranging frequency to 90Hz
+driver.set_ranging_frequency(90)
+
+# Set integration time to 2ms
+driver.set_integration_time(2)
+
+# Set ranging mode to continuous
+driver.set_ranging_mode(driver.VL53L5CX_RANGING_MODE_CONTINUOUS)
 
 # Ranging:
 driver.start_ranging()
 
-previous_time = 0
-loop = 0
-while loop < 10:
+prev_time = 0.0
+curr_time = time.time()
+while True:
     if driver.check_data_ready():
         ranging_data = driver.get_ranging_data()
 
         # As the sensor is set in 4x4 mode by default, we have a total 
         # of 16 zones to print. For this example, only the data of first zone are 
         # print
-        now = time.time()
-        if previous_time != 0:
-            time_to_get_new_data = now - previous_time
-            print(f"Print data no : {driver.streamcount: >3d} ({time_to_get_new_data * 1000:.1f}ms)")
-        else:
-            print(f"Print data no : {driver.streamcount: >3d}")
 
-        for i in range(16):
-            print(f"Zone : {i: >3d}, "
-                  f"Status : {ranging_data.target_status[driver.nb_target_per_zone * i]: >3d}, "
-                  f"Distance : {ranging_data.distance_mm[driver.nb_target_per_zone * i]: >4.0f} mm")
+        arr = numpy.flipud(numpy.array(ranging_data.distance_mm)).astype('float64')[0,0:16]
+        arr = arr.reshape((4, 4))
 
-        print("")
+        # Scale view relative to the furthest distance
+        distance = arr.max()
 
-        previous_time = now
-        loop += 1
+        # Scale view to a fixed distance
+        distance = 512
 
-    time.sleep(0.005)
+        # Scale and clip the result to 0-255
+        arr *= (255.0 / distance)
+        arr = numpy.clip(arr, 0, 255)
+
+        # Force to int
+        arr = arr.astype('uint8')
+        print(arr)
+
+        curr_time = time.time()
+        FPS = 1 / (curr_time - prev_time)
+        print("\nFPS: %0.2f" % FPS,"\n")
+        prev_time = curr_time
+        time.sleep(0.001)  # Avoid polling *too* fast
+
+        
+
+    
